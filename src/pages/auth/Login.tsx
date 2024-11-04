5a412e73e54c6a44a903e453945bd0ac30a95c93
rovesea1@gmail.com <rovesea1@gmail.com>
2024-11-04 23:23:31 +0700

    a

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,70 +1,86 @@
 import { useState } from "react";
-import axios from "axios";
 import { toast } from "react-toastify";
 import useEcomStore from "../../store/e-com-store";
+import { useNavigate } from "react-router-dom";
 
 interface form {
-  email: string,
-  password: string
+  email: string;
+  password: string;
 }
 
 const Login = () => {
-
-  const actionLogin = useEcomStore((state) => state.actionLogin)
+  const navigate = useNavigate()
+  const actionLogin = useEcomStore((state) => state.actionLogin);
+  // const user = useEcomStore((state) => state.user);
+  // console.log(user);
 
   const [form, setForm] = useState<form>({
-    email:"",
-    password:"",
-  })
+    email: "",
+    password: "",
+  });
 
-  const handleOnChange = (e:any) => {
+  const handleOnChange = (e: any) => {
     // console.log(e.target.name, e.target.value);
     setForm({
       ...form,
-      [e.target.name]:e.target.value
-    })    
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleSubmit = async () => {
-    
-    if(!form.email && !form.password){
-      return toast.error("Email and Password is Required")
+    if (!form.email && !form.password) {
+      return toast.error("Email and Password is Required");
     }
-    if(!form.email){
-      return toast.error("Email is Required")
+    if (!form.email) {
+      return toast.error("Email is Required");
     }
-    if(!form.password){
-      return toast.error("Password is Required")
+    if (!form.password) {
+      return toast.error("Password is Required");
     }
-    // console.log(form);    
+    // console.log(form);
     try {
-      const res:any = await actionLogin(form)
-      toast.success(res.data?.message)
+      const res:any = await actionLogin(form);
+      toast.success(res.data?.message);
       // console.log(res);
-      localStorage.setItem('accessToken', res.data.token);
-      localStorage.setItem('refreshToken', res.data.refreshToken);
-      
-      
-    } catch (err:any) {
-      // console.log(err.response?.data?.message);
-      
-      const errMsg = err.response?.data?.message;
-      toast.error(errMsg)
-    }
-  }
+      const role = res.data.user.role
+      // console.log(role);
 
+      if(role === 'admin') {
+        navigate('/admin')
+      } else {
+        navigate('/user')
+      }
+
+            
+    } catch (err: any) {
+      // console.log(err.response?.data?.message);
+
+      const errMsg = err.response?.data?.message;
+      toast.error(errMsg);
+    }
+  };
 
   return (
     <div>
       Login
       <form onSubmit={(e) => e.preventDefault()}>
         <label htmlFor="email">Email</label>
-        <input className="border" type="email" name="email" onChange = {handleOnChange}/>
-
+        <input
+          className="border"
+          type="email"
+          name="email"
+          onChange={handleOnChange}
+        />
         <label htmlFor="password">Password</label>a
-        <input type="password" name="password" className="border" onChange = {handleOnChange}/>
-
-        <button className="bg-blue-500 rounded-md" onClick={handleSubmit}>Login</button>
+        <input
+          type="password"
+          name="password"
+          className="border"
+          onChange={handleOnChange}
+        />
+        <button className="bg-blue-500 rounded-md" onClick={handleSubmit}>
+          Login
+        </button>
       </form>
     </div>
   );
--- src/store/e-com-store.tsx
+++ src/store/e-com-store.tsx
@@ -1,21 +1,41 @@
 import axios from "axios";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-interface formLogin {
+interface FormLogin {
   email: string;
   password: string;
 }
 
-const ecomStore = (setState: any) => ({
-  name: null,
-  value: null,
-  actionLogin: async (form: formLogin) => {
-    const res = await axios.post("http://localhost:3000/api/login", form);
-    
-    return res;
+
+const ecomStore = (set: any) => ({
+  user: null,
+  token: null,
+  refreshToken: null,
+  actionLogin: async (form: FormLogin) => {
+    try {
+      const res = await axios.post("http://localhost:3000/api/login", form);
+      // console.log(res);
+      
+      set({
+        user: res.data.user,
+        token: res.data.token,
+        refreshToken: res.data.refreshToken,
+      });
+
+      return res
+    } catch (error) {
+      console.error("Login failed:", error);
+      throw error; // rethrow if you want to handle it in the component
+    }
   },
 });
 
-const useEcomStore = create(ecomStore);
+const useEcomStore = create(
+  persist(ecomStore, {
+    name: "ecom-store",
+    storage: createJSONStorage(() => localStorage),
+  })
+);
 
 export default useEcomStore;
